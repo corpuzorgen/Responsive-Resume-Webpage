bf6ef2ccea4bf8926abd1fb0ba2430c496acb660
Orgen Corpuz <orgencorpuz@gmail.com>
2016-02-25 21:32:30 +0800

    Added files via upload
    Feel free to enhance the design and add functionality/animation with it.

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<meta name="description" content="Orgen Corpuz is a freelance Front-End Developer and SEO Specialist who is seeking for a long term job position in a company.">
+		<meta name="keywords" content="Front-End Developer,SEO Specialist,Orgen Corpuz">
+		<meta name="author" content="Orgen Dlanor M. Corpuz">
+		<title>Resume - Orgen Corpuz</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div class="container-fluid">
+			<header class="row">
+				<div class="col-md-12">
+					<h1 class="text-center" id="author">Orgen <span id="corpuz">Corpuz</span></h1>
+				</div>
+				<div class="col-md-12">
+					<h2 class="text-center" id="position">Front-End Developer &amp; SEO Specialist</h2>
+				</div>
+			</header>
+			<main class="row">
+				<div class="container" id="content">
+					<div class="col-md-12" id="summary">
+						<div class="col-md-12">
+							<h3 class="text-uppercase">Summary</h3>
+							<hr />
+							<div class="resumeContent col-md-offset-1">
+								<ul>
+									<li>Implement best practices on responsive designs, semantics, seo/sem.</li>
+									<li>Reliable, motivated, hardworking, and attentive to details.</li>
+									<li>Adaptive to new applications and technologies.</li>
+									<li>Strong analytical and troubleshooting skills.</li>
+									<li>Excellent communicator, both written and oral.</li>
+
+								</ul>
+							</div>	
+						</div>
+					</div>
+					<div class="col-md-12" id="expertise">
+						<div class="col-md-12">
+							<h3 class="text-uppercase">Technical Expertise</h3>
+							<hr />
+							<div class="resumeContent col-md-offset-1">
+								<div class="col-md-12">
+									<div class="col-md-4">
+										<h4>Languages</h4>
+										<ul id="languageslist">
+											<li>HTML5</li>
+											<li>CSS3</li>
+											<li>JavaScript, jQuery</li>
+											<li>PHP</li>
+											<li>MySQLi</li>
+										</ul>
+									</div>
+									<div class="col-md-4">
+										<h4>Tools</h4>
+										<ul id="toolslist">
+											<li>Github</li>
+											<li>Stack Overflow</li>
+											<li>Sublime Text</li>
+											<li>Chrome Dev Tool</li>
+											<li>Adobe Photoshop</li>
+											<li>Adobe Illustrator</li>
+											<li>Microsoft Office</li>
+										</ul>
+									</div>
+									<div class="col-md-4">
+										<h4>Platform</h4>
+										<ul id="platformlist">
+											<li>Linux Ubuntu &amp; Android</li>
+											<li>Microsoft Windows XP/7/8.1/10</li>
+										</ul>
+									</div>
+								</div>
+								<div class="col-md-12">
+									<div class="col-md-4">
+										<h4>Framework &amp; CMS</h4>
+										<ul id="frameworkcmslist">
+											<li>Twitter Bootstrap</li>
+											<li>Materialize CSS</li>
+											<li>Wordpress</li>
+											<li>Shopify</li>
+											<li>Hubspot</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12" id="workexperience">
+						<div class="col-md-12">
+							<h3 class="text-uppercase">Work Experience</h3>
+							<hr />
+							<div class="resumeContent col-md-offset-1">
+								<div class="col-md-12">
+									<h4>Professional</h4>
+									<dl class="col-md-offset-1 dl-horizontal">
+										<dt>Upwork.com <br /> 2015 - present</dt>
+										<dd>Freelance Front-End Developer and SEO Specialist. Have already done 3 jobs, SEO related which was hourly paid.
+										</dd>
+									</dl>
+								</div>
+								<div class="col-md-12">
+									<h4>Training</h4>
+									<dl class="col-md-offset-1 dl-horizontal">
+										<dt>My Creative Panda <br /> 2016 - present</dt>
+										<dd>Training as a Wordpress Developer, trained to know about Cakephp, AngularJS. 
+ 										</dd>
+									</dl>
+								</div>
+								<div class="col-md-12">
+									<h4>Case Study</h4>
+									<dl class="col-md-offset-1 dl-horizontal">
+										<dt>Tarlac First Baptist Church School</dt>
+										<dd>A website for TFBCS where updates are being posted with portal accounts for Teachers and Students. Wherein, teachers
+											can update students performances based on grades and students can view it on their account. Built from scratch using Bootstrap,
+											jQuery, AJAX, JSON, PHP, MYSQL.
+										</dd>
+									</dl>
+									<dl class="col-md-offset-1 dl-horizontal">
+										<dt>SMS Gateway</dt>
+										<dd>A one page website which users(PH based) are able to send text messages. Used <a href="https://smsgateway.me/">smsgateway.me</a>
+											as a server and used my phone's simcard as a way to send the messages to the recipients. Basically, we need the server and a phone
+											w/ load subscription to be used. Built from scratch using Bootstrap, jQuery, PHP.
+										</dd>
+									</dl>
+								</div>
+							</div>	
+						</div>
+					</div>
+					<div class="col-md-12" id="education">
+						<div class="col-md-12">
+							<h3 class="text-uppercase">Education</h3>
+							<hr />
+							<div class="resumeContent col-md-offset-1">
+								<p class=""><b>Bachelor of Science in Information Technology</b></p>
+								<p class="">Tarlac State University, 2012 - 2016</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12" id="contact">
+						<div class="col-md-12">
+							<h3 class="text-uppercase">Contact Me</h3>
+							<hr />
+							<div class="resumeContent col-md-offset-1">
+								<div class="col-md-6">
+									<address>
+									  <strong>Address</strong><br>
+									 Block 4 Champaca Street, San Vicente<br>
+									  Tarlac, Philippines 2300<br>
+									</address>
+									<address>
+									  <strong>Phone Number</strong><br>
+									 <abbr title="Phone">P:</abbr> +(63)905-965-5128
+									</address>
+									<address>
+									  <strong>Email Address</strong><br>
+									 <a href="mailto:#">orgencorpuz@gmail.com</a>
+									</address>	
+								</div>
+								<div class="col-md-6">
+									<h4>Facebook: <span><a href="https://www.facebook.com/orgen.corpuz" target="_blank">view profile</a></span></h4>
+									<h4>LinkedIn: <span><a href="https://ph.linkedin.com/in/orgencorpuz" target="_blank">view profile</a></span></h4>
+									<h4>Skype: <span>mawi_rodj</span></h4>
+								</div>								
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+			<footer class="row" id="footerresume">
+				<div class="container">
+					<h2 class="text-center">Hire me!</h2>
+				</div>
+			</footer>
+		</div>
+
+
+		<script src="js/jquery.1.12.0.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/script.js"></script>
+	</body>
+</html>
